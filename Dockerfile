9d88291632baf3d0d7050889ec70206f4c2232ce
Berdimurat Masaliev <muratmbt@gmail.com>
2019-06-07 14:43:55 +0600

    Add configuration templates

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM alpine:latest
 
 RUN apk add pgbouncer
 
-ADD entrypoint.sh /
+COPY entrypoint.sh /
+COPY pgbouncer.ini.tmpl /etc/pgbouncer/pgbouncer.ini.tmpl
+COPY userlist.txt.tmpl /etc/pgbouncer/userlist.txt.tmpl
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-adduser postgres
 mkdir -p /var/log/pgbouncer
 chmod -R 755 /var/log/pgbouncer
 chown -R postgres:postgres /var/log/pgbouncer
+
+POSTGRES_DB=${POSTGRES_DB:-postgres}
+POSTGRES_USER=${POSTGRES_USER:-postgres}
+POSTGRES_HOST=${POSTGRES_HOST:-postgres}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+
+cp /etc/pgbouncer/userlist.txt.tmpl /etc/pgbouncer/userlist.txt
+sed -i "s/<POSTGRES_USER>/$POSTGRES_USER/g" /etc/pgbouncer/userlist.txt
+sed -i "s/<POSTGRES_PASSWORD>/$POSTGRES_PASSWORD/g" /etc/pgbouncer/userlist.txt
+
+cp /etc/pgbouncer/pgbouncer.ini.tmpl /etc/pgbouncer/pgbouncer.ini
+sed -i "s/<POSTGRES_DB>/$POSTGRES_DB/g" /etc/pgbouncer/pgbouncer.ini
+sed -i "s/<POSTGRES_USER>/$POSTGRES_USER/g" /etc/pgbouncer/pgbouncer.ini
+sed -i "s/<POSTGRES_HOST>/$POSTGRES_HOST/g" /etc/pgbouncer/pgbouncer.ini
+sed -i "s/<POSTGRES_PASSWORD>/$POSTGRES_PASSWORD/g" /etc/pgbouncer/pgbouncer.ini
+sed -i "s/<POSTGRES_PORT>/$POSTGRES_PORT/g" /etc/pgbouncer/pgbouncer.ini
 
 echo "Starting pgbouncer..."
 exec /usr/bin/pgbouncer -q -u postgres /etc/pgbouncer/pgbouncer.ini
